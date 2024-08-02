efe16547d5d5855954159ec9b406ddd4835aefbd
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-02 15:22:27 +0200

    Enhancing the Hello World REST API to Return a Bean

---
+++ src/main/java/com/dgmf/entity/HelloWorldBean.java
@@ -0,0 +1,11 @@
+package com.dgmf.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class HelloWorldBean {
+    private String message;
+}
